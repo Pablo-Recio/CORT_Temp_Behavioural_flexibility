134af9d114fd36ee749acb9ac3b563bad65e3a9f
Pablo-Recio <93.pablo.recio@gmail.com>
2024-01-31 18:14:50 +1100

    Updates

--- R/1_data_process.R
+++ R/1_data_process.R
@@ -1,43 +1,25 @@
 ####################################
 # 1_data_process
 ####################################
-
 # Packages
 pacman::p_load(tidyverse, flextable, emmeans, DHARMa, brms, here, ggplot2, lme4, zoo, lmerTest, broom, forcats)
-
+#
 # Load data
 data  <-  read.csv("./data/Learning.csv")
-
-# Remove individuals who did not participate (more than 15 NAs), remove trials [36-40] (Only in associative) and split treatment into Temp and Cort
-data_asso <- data %>%
+###### Select individuals that pass the criterion of 80% of correct choices in the last 10 trials in the Associative
+#
+# Calculate the percentage of '1' values in FC_associative for the last 10 trials
+percentage_per_lizard <- data %>%
+  filter(trial_associative > 25 & trial_associative <= 35) %>%
   group_by(lizard_id) %>%
-    filter(sum(is.na(FC_associative)) <= 15) %>%
-    filter(trial_associative <= 35) %>%
-  ungroup()  %>%
-    mutate(group = factor(group,
-     levels = c("R_B", "B_R"),
-     labels=c("R_B"="Red", "B_R"="Blue"))) %>%
-    mutate(temp = gsub("[AB]_", "", trt),
-          cort = gsub("_[2][38]", "", trt))  %>%
-    mutate(temp = factor(temp,
-      levels = c("23", "28"),
-      labels = c("23" = "Cold", "28" = "Hot"))) %>%
-    mutate(cort = factor(cort,
-      levels = c("B", "A"),
-      labels = c("B" = "CORT", "A" = "Control"))) %>%
-    mutate( # Standarize data by trial (i.e. make the first trial where each individual participated their trial 1)
-      first_non_na = min(which(!is.na(FC_associative))),
-      Associative_Trial = ifelse(!is.na(first_non_na),trial_associative - first_non_na + 1, trial_associative))%>% 
-      filter(Associative_Trial >= 1) %>%
-  ungroup() %>% 
-data.frame()
-
-
-data_rev <- data %>%
-  group_by(lizard_id) %>%
-    filter(sum(is.na(FC_associative)) <= 15) %>%
-    filter(sum(is.na(FC_reversal)) <= 15) %>%
-   ungroup()  %>%
+  summarise(percentage = mean(FC_associative == 1))
+# Filter lizards where the percentage is at least 80%
+filtered_lizards <- percentage_per_lizard %>%
+  filter(percentage >= 0.8)
+# Subset the original dataset based on the filtered lizard IDs
+clean_df <- data %>%
+  filter(lizard_id %in% filtered_lizards$lizard_id) %>%
+  filter(sum(is.na(FC_reversal)) <= 15) ungroup()  %>%
     mutate(group = factor(group,
      levels = c("R_B", "B_R"),
      labels=c("R_B"="Red", "B_R"="Blue"))) %>%
@@ -51,5 +33,6 @@ data_rev <- data %>%
       labels = c("B" = "CORT", "A" = "Control"))) %>%
     mutate(trial_reversal=as.numeric(trial_reversal)) %>%
   data.frame()
+write.csv(filtered_df, "./output/databases_clean/clean_database.csv")
 
 
