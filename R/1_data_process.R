bddaebdbe72f540e8f35fd4b0c58314dc6de608d
Pablo-Recio <93.pablo.recio@gmail.com>
2024-02-02 11:02:16 +1100

    Updates

--- R/1_data_process.R
+++ R/1_data_process.R
@@ -6,19 +6,10 @@ pacman::p_load(tidyverse, flextable, emmeans, DHARMa, brms, here, ggplot2, lme4,
 #
 # Load data
 data  <-  read.csv("./data/Learning.csv")
-###### Select individuals that pass the criterion of 80% of correct choices in the last 10 trials in the Associative
 #
-# Calculate the percentage of '1' values in FC_associative for the last 10 trials
-percentage_per_lizard <- data %>%
-  filter(trial_associative > 25 & trial_associative <= 35) %>%
-  group_by(lizard_id) %>%
-  summarise(percentage = mean(FC_associative == 1))
-# Filter lizards where the percentage is at least 80%
-filtered_lizards <- percentage_per_lizard %>%
-  filter(percentage >= 0.8)
 # Subset the original dataset based on the filtered lizard IDs
 clean_df <- data %>%
-  filter(lizard_id %in% filtered_lizards$lizard_id) %>%
+  group_by(lizard_id) %>%
   filter(sum(is.na(FC_reversal)) <= 15) %>%
     mutate(temp = gsub("[AB]_", "", trt),
           cort = gsub("_[2][38]", "", trt))  %>%
@@ -31,5 +22,4 @@ clean_df <- data %>%
     mutate(trial_reversal=as.numeric(trial_reversal)) %>%
   data.frame()
 write.csv(clean_df, "./output/databases_clean/clean_df.csv")
-
 
